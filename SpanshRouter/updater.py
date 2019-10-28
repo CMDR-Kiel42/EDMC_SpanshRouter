24e15111b80a8a0a334eb48ddbd8962d8ef1fc2b
Kiel42 <klideyla@gmail.com>
2019-10-29 00:23:20 +0100

    Fetch the changelogs and display them in a popup asking if we should install

--- SpanshRouter/updater.py
+++ SpanshRouter/updater.py
@@ -13,6 +13,7 @@ class SpanshUpdater():
         self.plugin_dir = plugin_dir
         self.zip_path = os.path.join(self.plugin_dir, self.zip_name)
         self.zip_downloaded = False
+        self.changelogs = ""
 
     def download_zip(self):
         url = 'https://github.com/CMDR-Kiel42/EDMC_SpanshRouter/releases/download/v' + self.version + '/' + self.zip_name
@@ -46,3 +47,17 @@ class SpanshUpdater():
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
                 sys.stderr.write(''.join('!! ' + line for line in lines))
+
+    def get_changelog(self):
+        url = "https://raw.githubusercontent.com/CMDR-Kiel42/EDMC_SpanshRouter/master/CHANGELOG.md"
+        try:
+            r = requests.get(url, timeout=2)
+            
+            if r.status_code == 200:
+                start_index = r.content.find("## " + self.version)
+                stop_index = r.content.find("##", start_index + 2)
+                self.changelogs = r.content[start_index: stop_index:]
+        except:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            sys.stderr.write(''.join('!! ' + line for line in lines))
